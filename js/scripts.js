52536f00835f80f942c601df9b3f59ed6aa8764d
Matt <matt@DontTryisAtHome.broadband>
2019-10-30 20:52:31 +0000

    Add floating menu

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,18 @@ $(document).ready(function() {
 	});
 */
 
+	$(window).scroll(function(){                          
+        if ($(this).scrollTop() > 300) {
+            $('#navbar-pagenav').fadeIn(200);
+        } else {
+            $('#navbar-pagenav').fadeOut(200);
+        }
+    });
+
+    $( "a.scrollLink" ).click(function( event ) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
 
 });
 
