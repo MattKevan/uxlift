593e5decf4bd397568ec354f58dd4d1aa5f06529
Matt <matt@GBBAL13096.broadband>
2020-06-10 13:52:20 +0100

    Remove old functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
- 
-
 	$(window).scroll(function(){                          
         if ($(this).scrollTop() > 250) {
             $('#navbar-pagenav').fadeIn(200);
@@ -14,12 +12,6 @@ $(document).ready(function() {
         event.preventDefault();
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
-    
-    $(document).on('click','.navbar-collapse.in',function(e) {
-    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
-        $(this).collapse('hide');
-    }
-});
 
 });
 
