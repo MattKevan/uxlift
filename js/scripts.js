f556faaad814ceb5b32f336c1cd78364f26ca919
Matt <matt@GBBAL13096.broadband>
2020-08-21 10:45:50 +0100

    Add filters to publications page

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,29 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
     });
 
+     // clicking button with class "category-button"
+    $(".category-button").click(function(){
+        // get the data-filter value of the button
+        $('.active').not($(this)).removeClass('active');
+        $(this).toggleClass('active');
+
+        var filterValue = $(this).attr('data-filter');
+        
+        // show all items
+        if(filterValue == "all")
+        {
+            $(".all").show("slow");
+        }
+        else
+        {   
+            // hide all items
+            $(".all").not('.'+filterValue).hide("slow");
+            // and then, show only items with selected data-filter value
+            $(".all").filter('.'+filterValue).show("slow");
+        }
+    });
+
+
 });
 
 
